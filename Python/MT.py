9c3aa1dfeb22f23cdfac5558f90655ef315838fb
mario-lestaire <mario.lestaire@hotmail.com>
2022-09-27 19:17:00 +0200

    commit

--- Python/MT.py
+++ Python/MT.py
@@ -1,17 +1,63 @@
+import re
+import array
+
+
+# TIPOS de caracteres que pueden entrar
+letras = list(range(97,122+1)) + list(range(65,90+1))
+digitos = range(0,9+1)
+#32=espacio 
+delimitadores = [32]
+palabrasReservadas = ["switch", "case", "default", "break", "let", "int", 
+                    "boolean", "string", "if", "function", 
+                    "input", "print", "return" "eof"]
+c1 = list(range(33,42)) + list(range(43,126+1)) 
+c2 = list(range(33,42)) + list(range(43,47)) + list(range(48,126+1))
+c3 = list(range(33,34)) + list(range(35,126+1))
+
+for n in letras:
+    print(chr(n))
+
+for n in digitos:
+    print(n)
+
+
+# Funcion: saber si la palabra es reservada
+def esreservada(palabra):
+    esreservada= False
+    for p in palabrasReservadas:
+        if(p==palabra):
+            esreservada = True
+    
+    return esreservada
+
+
+# ***MATRIZ AFD***
+lexema="" #Vaciarlo cuando se genere el token 
+valor=0   # ""
+
 def MatrizTransicciones(e,c):
 
     accion=0
     estadoSig=e
-    letras =[a_zA_Z]
-    
+
+    #----------CASILLAS MATRIZ AFD---------
     # S
     if e =="S":
-        if c == 104:
-            accion = 1
-            estadoSig = "A"
-        if c == 97:
+        if c in letras:
             accion = 2
-            estadoSig = "B"
+            estadoSig = "A"
+        if c in digitos:
+            accion = 5
+            estadoSig = "C"
+        if c == 95:
+            accion = 6
+            estadoSig = "C"
+        if c == 34:
+            accion = 9
+            estadoSig = "K"
+        if c == 47:
+            accion = 8
+            estadoSig = "E"  #-->--->---->POR AQUI<-----<----<--
     
 
     # A
@@ -24,7 +70,6 @@ def MatrizTransicciones(e,c):
             estadoSig = "Otra"
 
     # B
-    # A
     # C
     # D
     # E
@@ -52,3 +97,6 @@ def MatrizTransicciones(e,c):
 
     
     return accion,estadoSig, "hola"
+
+
+print("funciona")
