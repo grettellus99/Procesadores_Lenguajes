804435287154e69fc5916be0b800b88dd425295e
mario-lestaire <mario.lestaire@hotmail.com>
2022-10-05 00:07:17 +0200

    commit

--- Python/main.py
+++ Python/main.py
@@ -1,38 +1,282 @@
+from asyncio.windows_events import NULL
+import re
 import array
 
 
-
+#----------------------TOKENS-------------------------
 class Token:
     def __init__(self):
             self.nombre = "variable"
             self.valor = 0
 
-def MatrizTransicciones(e,c):
-    accion=0
-    estadoSig=e
-    if e =="S":
-        if c == 104:
-            accion = 1
-            estadoSig = "A"
-        if c == 97:
-            accion = 2
-            estadoSig = "B"
-    if e =="A":
-        if c == 111:
-            accion = 3
-            estadoSig = "C"
-        if c == 97:
-            accion = 4
-            estadoSig = "Otra"
+#-----------------ACCIONES SEMANTICAS--------------------
+def accionesSemanticas (a,ctr):
+    l=""
+    v=""
+    c=chr(ctr)
 
-    return accion,estadoSig, "hola"
+    if a == 0:
+        l= l + c
+    if a == 1:
+        v= v+ctr
+        
+
+
+    return NULL
+
+
+
+#----------Funcion: saber si la palabra es reservada-------
+def esreservada(palabra):
+    esreservada= False
+    for p in palabrasReservadas:
+        if(p==palabra):
+            esreservada = True
+    
+    return esreservada
+
+
+
+
+#-----------TRANSICIONES-----------
+def MatrizTransicciones(e,cct):
+    accion=0
+    estadoSig=""
+    estadoFinal = False
+    c=cct
+
+    
+    letras = list(range(65,90+1)) + list(range(97,122+1))
+    #CASILLAS MATRIZ AFD:
+    # S
+    if e =="S":
+        if c in letras:
+            accion = 2
+            estadoSig = "A"
+        if c in digitos:
+            accion = 5
+            estadoSig = "C"
+        if c == 95:       #if c == _
+            accion = 2
+            estadoSig = "A"
+        if c == 34:       #if c == ""
+            accion = 9
+            estadoSig = "K"
+        if c == 47:       #if c == /
+            accion = 8
+            estadoSig = "E"  
+        if c == 59:       #if c == ;
+            accion = 24
+            estadoSig = "W"
+        if c == 44:       #if c == , 
+            accion = 25
+            estadoSig = "X"
+        if c == 42:       #if c == *
+            accion = 15
+            estadoSig = "M"
+        if c == 43:       #if c == +
+            accion = 26
+            estadoSig = "Y"
+        if c == 38:       #if c == &
+            accion = 18
+            estadoSig = "P"
+        if c == 40:       #if c == (
+            accion = 20
+            estadoSig = "R"
+        if c == 41:       #if c == )
+            accion = 21
+            estadoSig = "T"
+        if c == 123:       #if c == {
+            accion = 22
+            estadoSig = "U"
+        if c == 125:       #if c == }
+            accion = 23
+            estadoSig = "V"
+        if c == 61:       #if c ==   =
+            accion = 12
+            estadoSig = "H"
+        if c in delimitadores:
+            accion = 21 
+            estadoSig = "S"
+        else:
+            print("El caracter", c," no es valido para el estado", e)
+    # A
+    if e =="A":
+        if c in letras:
+            accion = 3
+            estadoSig = "A"
+        if c in digitos:
+            accion = 3
+            estadoSig = "A"
+        if c == 95:       #if c == _
+            accion = 3
+            estadoSig = "A"
+        #OTHER CHARACTER CASO
+            #accion = 4
+            #estadoSig = "B"
+        else:
+            accion=4
+            estadoSig = "B"
+    # B
+    if e =="B":
+        estadoFinal= True
+        #vaciar Lexema y valor
+    
+
+    # C
+    if e =="C":
+        if c == 95:       #if c == _
+            accion = 6
+            estadoSig = "C"
+        else:
+            accion = 7
+            estadoSig = "D"
+
+    # D
+    if e =="D":
+        estadoFinal = True
+
+    # E
+    if e =="E":
+        if c == 42:       #if c == *
+            accion = 8
+            estadoSig = "F"
+        else:
+            print("El caracter", c," no es valido para el estado", e)
+
+    # F
+    if e =="F":
+        if c in c1:       #if c ==  C1
+            accion = 8
+            estadoSig = "F"
+        if c == 42:       #if c == *
+            accion = 8
+            estadoSig = "G"
+        else:
+            print("El caracter", c," no es valido para el estado", e)
+        
+    # G
+    if e =="G":
+        if c in c2:       #if c ==  C2
+            accion = 8
+            estadoSig = "F"
+        if c == 47:       #if c == /
+            accion = 8
+            estadoSig = "S"
+        if c == 42:       #if c == *
+            accion = 8
+            estadoSig = "G"
+        else:
+            print("El caracter", c," no es valido para el estado", e)
+
+    # H
+    if e =="H":
+        
+        if c == 41:       #if c ==   =
+            accion = 13
+            estadoSig = "I"
+        else:
+            accion= 14
+            estadoSig = "J"
+
+    # I
+    if e =="I":
+        estadoFinal = True
+    
+    # J
+    if e =="J":
+        estadoFinal = True
+
+    # K
+    if e =="K":
+        if c in c3:       #if c ==  C3
+            accion = 10
+            estadoSig = "K"
+        if c == 34:       #if c == ""
+            accion = 11
+            estadoSig = "L"
+        else:
+            print("El caracter", c," no es valido para el estado", e)
+
+        
+    # L
+    if e =="L":
+        estadoFinal = True
+
+
+    # M
+    if e =="M":
+
+        if c == 41:       #if c ==   =
+            accion = 16
+            estadoSig = "N"
+        else:
+            accion = 17
+            estadoSig = "O"
+    
+    
+    # N
+    if e =="N":
+        estadoFinal = True
+
+    # O
+    if e =="O":
+        estadoFinal = True
+    
+    # P
+    if e =="P":
+        if c == 38:       #if c == &
+            accion = 19
+            estadoSig = "Q"
+        else:
+            print("El caracter", c," no es valido para el estado", e)
+
+    # Q
+    if e =="Q":
+        estadoFinal = True
+
+    # R
+    if e =="R":
+        estadoFinal = True
+
+    # T
+    if e =="T":
+        estadoFinal = True
+
+    # U
+    if e =="U":
+        estadoFinal = True  
+
+    # V
+    if e =="V":
+        estadoFinal = True
+
+    # W
+    if e =="W":
+        estadoFinal = True
+
+    # X
+    if e =="X":
+        estadoFinal = True
+
+    # Y
+    if e =="Y":
+        estadoFinal = True
+    
+    #Z
+    if e == "Z":
+        estadoFinal = True
 
 
     
-#LEER CARACTER
+    return accion,estadoSig, estadoFinal
+
+
+    
+#-----------LEER CARACTER-----------
 pos=0
 def leerCaracter (t,p):
-    c = ord(texto[p])
+    c = ord(t[p])
     global pos
     pos = pos+1
     return c
@@ -40,25 +284,37 @@ def leerCaracter (t,p):
 
 
 
+
+
+
+#------------------------DATOS------------------------
+
+#-------TIPOS de caracteres que pueden entrar  
+digitos = range(0,9+1)
+#32=espacio 
+delimitadores = [32]
+palabrasReservadas = ["switch", "case", "default", "break", "let", "int", 
+                    "boolean", "string", "if", "function", 
+                    "input", "print", "return" "eof"]
+c1 = list(range(33,42)) + list(range(43,126+1)) 
+c2 = list(range(33,42)) + list(range(43,47)) + list(range(48,126+1))
+c3 = list(range(33,34)) + list(range(35,126+1))
+
 token = Token()
+#Programa a compilar
 texto = input("CÓDIGO: ")
+#Inicializar 
 estadoInicial ="S"
 print("Elemento posicion= ",pos)
 caracterLeido = leerCaracter(texto,pos)
 
-"""
-transiccion = MatrizTransicciones(estadoInicial,caracterLeido)
-estadoActual = transiccion[1]
-accionARealizar = transiccion[0]
-print(transiccion)
-caracterLeido = leerCaracter(texto,pos)
+#Para los TOKENS
+lexema =""
+valor = 0
 
 
-transiccion = MatrizTransicciones(estadoActual,caracterLeido)
-print(transiccion)
-"""
 
-#TRANSICIONES
+#------------------BUCLE TRANSICIONES-------------------
 transiccion = MatrizTransicciones(estadoInicial,caracterLeido)
 for x in texto:
     print(caracterLeido)
@@ -76,11 +332,5 @@ for x in texto:
     transiccion = MatrizTransicciones(estadoActual,caracterLeido)
 
 
-
-
-
-
-print ("token: ", token.nombre)
-print ("valor: ", token.valor)
 
 
--- Python/pruebas.py
+++ Python/pruebas.py
@@ -2,7 +2,7 @@ from ast import List
 import re
 import array
 
-"""
+""" pruebas valores caraceteres
 letras = list(range(97,122+1)) + list(range(65,90+1))
 
 for n in letras:
@@ -28,6 +28,7 @@ def esreservada(palabra):
     
     return esreservada
 """
+"""
 c1 = list(range(33,42)) + list(range(43,126+1)) + list(range(130,131))
 c2 = list(range(33,42)) + list(range(43,47)) + list(range(48,126+1))
 c3 = list(range(33,34)) + list(range(35,126+1))
@@ -42,7 +43,114 @@ else:
     print("mal")
 
 print("xao")
+"""
+
+#---------CLASE TOKENS----------
+class Token():
+    def __init__(self, parte1, parte2):
+            self.p1 = parte1
+            self.p2 = parte2
+
+
+#-------Acciones semanticas:---------
+
+listaTokens = []
+
+def accionSemantica(a,ctr):
+
+    global lexema
+    global valor
+    global listaTokens
+
+    c=chr(ctr)
+
+    if a == 0:
+        print("Accion no valida") 
+    if a == 1:
+        print("leer")
+    if a == 2:
+        lexema= c
+    if a == 3:     
+        lexema= lexema + c
+    if a == 4:
+        lexema= c #POR HACER
+    if a == 5:
+        valor = c 
+    if a == 6:
+        valor= valor *10 + c
+    if a == 7:
+        if valor > 1000:
+            print("ERROR")
+        else:
+            print("genTOKEN  <cteEntera,valor>" )# <----------POR AQUI!!!!!!!!!
+             #POR probar
+
+            token = Token("hola", 3)
+
+            listaTokens.append(token)
+
+    if a == 8:
+        print("leer")
+    if a == 9:
+        lexema= "" 
+    if a == 10:
+        lexema= lexema + c
+    if a == 11:
+        if lexema.length() > 64:
+            print("ERROR")
+        else:
+            print("genTOKEN  <cadena,lexema>")
+    if a == 12:
+        print("leer")
+    if a == 13:
+        print("genTOKEN  <asig,->")
+    if a == 14:
+        print("genTOKEN  <opRelacional,1>")
+    if a == 15:
+        print("leer")
+    if a == 16:
+        print("genTOKEN  <asigMult,->")
+    if a == 17:
+        print("genTOKEN  <opAritmetico,1>")
+    if a == 18:
+        print("leer")
+    if a == 19:
+        print("genTOKEN  <opLogico,1>")
+    if a == 20:
+        print("genTOKEN  <abrirParantesis,->")
+    if a == 21:
+        print("genTOKEN  <cerrarParantesis,->")
+    if a == 22:
+        print("genTOKEN  <abrirCorchete,->")
+    if a == 23:
+        print("genTOKEN  <cerrarCorchete,->")
+    if a == 24:
+        print("genTOKEN  <ptoComa,->")
+    if a == 25:
+        print("genTOKEN  <coma,->")
+    if a == 26:
+        print("genTOKEN  <opAritmetico,2>")
 
 
 
+lexema = ""
+valor = 0
+
+nacc=2
+caracter = 104
+
+print("LEXEMA:",lexema)
+accionSemantica(nacc,caracter)
+
+nacc=3
+caracter= 105
+print("LEXEMA:",lexema)
+accionSemantica(nacc,caracter)
+print("LEXEMA:",lexema)
+
+tokenPrueba = Token("hola","-")
+listaTokens.append(tokenPrueba)
+
+print(listaTokens[0].p1)
+print(listaTokens[0].p2)
 
