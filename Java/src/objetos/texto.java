09598f23e40d5ed1adb6a0b5a5ec9fcd8d852652
mario-lestaire <mario.lestaire@hotmail.com>
2022-10-11 15:43:42 +0200

    commit

---
+++ Java/src/objetos/texto.java
@@ -0,0 +1,9 @@
+package objetos;
+import java.util.*;
+
+public class texto {
+    
+    Scanner sc = new Scanner(System.in);
+    
+
+}
---
+++ Python/Analizadores/Léxico/AnalizadorLexico.py
@@ -0,0 +1,6 @@
+from tokenize import Token
+from MT.py import *
+
+token = Token("hola", 3)
+
+print(token)
