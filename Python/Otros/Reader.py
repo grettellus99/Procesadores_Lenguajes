034d6ea5e6313cea2d83123df39910eab7fe3569
Grettell Umpierrez <grettellus99@gmail.com>
2022-10-12 12:03:45 +0200

    Reader v1

--- Python/Otros/Reader.py
+++ Python/Otros/Reader.py
@@ -1,15 +1,22 @@
 import os.path
+
 class Reader:
     
     ### Constructor del Reader que necesita un path a un archivo ####
     ### Guarda la referencia al archivo abierto si este está disponible ###
-    def _init_ (self, f_path):
-        self.path=f_path
-        self.numLinea=0;
-        self.numCar=0;
+    def __init__ (self, f_path):
+       
+        dirname=os.path.dirname(__file__)
+        filename=os.path.join(dirname,f_path)
+       
+        self.path=filename
+        self.numLinea=0
+        self.numCar=0
+        self.linea=""
+        
         try:
             if(os.path.exists(self.path)):  # el path introducido existe?
-                f = open(f_path)    # abrir el archivo
+                f = open(self.path)    # abrir el archivo
                 if(f.readable()):   # permiso de lectura?
                     self.file=f        # atributo file de objeto apunta al archivo abierto
                 else:
@@ -20,56 +27,68 @@ class Reader:
             print("Error: {0}",format(err))
 
     ### Devuelve la linea numero numLinea de un archivo
-    def readLine():
-        f=Reader.file
-        linea="";
-        i=0
+    def readLine(self,endFile):
+        
+        f=self.file
+        linea=""
         try:
-            while i<Reader.numLinea: 
-                linea = f.readline() #leer la lineas hasta llegar a la pedida
-                i+=1
-            Reader.numLinea +=1
-            linea = f.readline() #la linea pedida
-        except OSError as err:
-            print("Error: {0}",format(err))
-        
-        return linea #retornar la linea pedida
-    
-        ### MALLL ###
-    def readSigCaracter(endFile):
-        endFile=False
-        try:
-            linea=Reader.readLine() 
-        except OSError as err:
-            print("Error: {0}",format(err))
-        #if()
-        c=linea[Reader.numCar]
-        if(c=="/0" | c==3 ):
-            endFile=True
-        
-        if(endFile):
-            return endFile
-        else:
-            Reader.numCar+=1
-            return c
-            
-        
+            linea = f.readline()    # la linea pedida
+            self.numLinea +=1     # se leyó una línea
+             
+            if(linea!=""):
+                self.linea = linea    # retornar la linea pedida
+            else:
+                endFile=True    
+            return endFile       
 
-              
+        except OSError as err:
+            print("Error: {0}",format(err))
     
+    # Devuelve el siguiente caracter. Si se acaba el fichero devuelve False      
+    def readSigCaracter(self):
+        endFile=False
+        i=self.numCar
+        l= self.linea
+        lengthLine= len(l)
+        
+        if(i>=lengthLine):
+            endFile = self.readLine(endFile)  # si la linea se agotó leer una nueva linea
+            if(endFile):
+                return False # si la nueva linea es eof devolver false
+            self.numCar=0
+        
+        i=self.numCar
+        l= self.linea   
+        c=ord(l[i])
+        self.numCar+=1    # pos sig caracter
+        return c
+        
     ## Cierra el fichero
-    def close ():
+    def close (self):
         closed= False
         try:
-            Reader.file.close()
+            self.file.close()
             closed=True 
         except OSError as err:
             print("Error: {0}",format(err))
         return closed
-    
-    
-       
-    ## Escribe en el fichero
-    #def write(texto):
-
-    
+        
+    ## Escribe en el fichero sustituyendo todo si sobreescribir es True
+    ## o agregando si sobrescribir es False
+    def write(self,texto,sobrescribir):
+        self.file.close()
+        try:
+            if(sobrescribir): 
+                # "w" permite sobrescribir todo
+                f=open(self.path,"w")
+                self.file=f
+                f.write(texto)
+                f.write('\n')       
+            else:
+                # "a" permite agregar una linea al final
+                f=open(self.path,"a")
+                self.file=f
+                f.write(texto)
+                f.write('\n')   
+        except OSError as err:
+            print("Error: {0}",format(err))
---
+++ Python/Otros/reader_test.py
@@ -0,0 +1,20 @@
+
+from reader import Reader
+
+r=Reader("../Ficheros Fuente/fichero_fuente.txt")
+
+
+while(True):
+    c = r.readSigCaracter()
+    if(c==False):
+        break;
+    print(c)
+   
+r.close()
+
+w=Reader("../Ficheros Salida/tokens.txt")
+
+w.write("<hola,->",True)
+w.write("<hola2,->",False)
+
+w.close()
