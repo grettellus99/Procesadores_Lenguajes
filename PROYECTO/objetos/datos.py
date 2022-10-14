48ae781989e4f54e59a7b2ee920265e8731f54a9
Grettell Umpierrez <grettellus99@gmail.com>
2022-10-14 20:52:06 +0200

    Agragar true y false PR

--- PROYECTO/objetos/datos.py
+++ PROYECTO/objetos/datos.py
@@ -6,7 +6,7 @@ digitos = range(48,57+1)
 #32=espacio
 palabrasReservadas = ["switch", "case", "default", "break", "let", "int", 
                     "boolean", "string", "if", "function", 
-                    "input", "print", "return","eof"]
+                    "input", "print", "return","eof","true","false"]
 c1 = list(range(32,42)) + list(range(43,255+1)) + list(range(10,11)) + list(range(13,14))
 c2 = list(range(32,42)) + list(range(43,47)) + list(range(48,255+1)) + list(range(10,11)) + list(range(13,14))
 c3 = list(range(32,34)) + list(range(35,255+1))
